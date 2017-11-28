76ddc538241eb9466da1bd8bfeb970107cee0839
Evan Ansell <evan.ansell@gmail.com>
2017-11-28 00:30:38 -0500

    Made a bit of progress with testing - may need to separate methods

--- src/TouchTime_3XA3/app/src/test/Java/touchtime_tests/vibCountTest.java
+++ src/TouchTime_3XA3/app/src/test/Java/touchtime_tests/vibCountTest.java
@@ -2,24 +2,52 @@ package touchtime_tests; /**
  * Created by Evan Ansell on 2017-11-26.
  */
 
+
+import android.support.wearable.watchface.CanvasWatchFaceService;
+
 import com.example.mattpo.touchtime_3xa3.TouchTime;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 public class vibCountTest {
 
+    int[] expected;
+    int[] result;
+
+    @Before
+    public void setUp() {
+        expected = new int[3];
+    }
+
+    @After
+    public void tearDown() {
+        expected = null;
+        result = null;
+    }
 
 
     @Test
     public void minTime() {
-        assertTrue(TouchTime.Engine.vibCalc(11,59).equals([11,5,9]));
+        expected[0] = 11; expected[1] = 5; expected[2] = 9;
+        result = TouchTime.vibCalc(11,59);
+        for (int i = 0; i < 2; i++){
+            assertEquals(expected[i], result[i]);
+        }
     }
 
     @Test
-    void maxTime() {
+    public void maxTime() {
         //TODO find out what the new pattern is for 12:00, see if hour is in system as 0 or 12
-        assertTrue(TouchTime.Engine.vibCalc(12,00).equals([?,0,0]));
+        expected[0] = ?; expected[1] = 0; expected[2] = 0;
+        result = TouchTime.vibCalc(0,00);
+        for (int i = 0; i < 2; i++){
+            assertEquals(expected[i], result[i]);
+        }
     }
 }
